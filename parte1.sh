db713e5698cc8319e51ee982daf32fba7f9d3bd7
Emi <nicovilaviviano@gmail.com>
2024-09-20 20:39:35 -0300

    Menu registrar usuario casi terminado (falta chequeo de usuario duplicado)

--- parte1.sh
+++ parte1.sh
@@ -34,17 +34,56 @@ login(){
     done 
 }
 
-optionUser(){
+option(){
     askOption=false
 
     while [[ $askOption == false ]] ; do 
-        echo "Opciones: "
-        echo "1. Listar mascotas disponibles en adopción"
-        echo "2. Adoptar mascota"
-        echo "3. Salir"
-
-        read option
-
+        if [[ $1 == true ]] ; then
+            echo -e "\nOpciones: "
+            echo "1. Registrar usuario"
+            echo "2. Registro de mascotas"
+            echo "3. Estadisticas de adopcion"
+            echo "4. Salir"
+            echo ""
+            read -p "Ingrese opcion: " option
+            if [[ $option == 1 ]] ; then
+                prueba="no"
+                while [[ $prueba == "no" ]] ; do
+                    echo -e "\nIngrese el nombre del nuevo usuario:"
+                    read newName
+                    echo -e "\nIngrese la cedula del nuevo usuario:"
+                    read newUser
+                    prueba=$(grep ^$newUser users.txt)
+                    if [[ $prueba == "" ]] ; then
+                        echo -e "\nIngrese el numero de telefono del nuevo usuario:"
+                        read newNum
+                        validDate=false
+                        while [[ $validDate == false ]] ; do
+                            echo -e "\nIngrese la fecha de nacimiento del nuevo usuario:"
+                            read newDate
+                            if [[ $newDate =~ ^([0-2][0-9]|3[0-1])/(0[1-9]|1[0-2])/[0-9]{4}$ ]] ; then
+                                validDate=true
+                            else
+                                echo -e "\nIngrese una fecha valida en formato dd/mm/aaaa"
+                            fi
+                        done
+                        echo -e "\nEl usuario fue registrado exitosamente"
+                    else
+                        echo -e "\nEl usuario no fue registrado"
+                    fi
+                done
+            elif [[ $option == 2 ]] ; then
+                echo ""
+            elif [[ $option == 3 ]] ; then
+                askOption=true
+            elif [[ $option == 4 ]] ; then
+                askOption=true
+            else
+                echo "error"
+            fi
+        else
+            echo "Menu user"
+        fi
 
     done
 }
@@ -53,13 +92,6 @@ echo "Bienvenido al sistema, inicie seción"
 
 while [[ $app == true ]] ; do
     login
-    if [[ $admin == true ]] ; then 
-
-    else 
-        
-        
-
-    fi
-
+    option $admin
     app=false
 done
