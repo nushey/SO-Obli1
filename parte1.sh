9387123e81cd6cdd9e9c2959e15d0f2457f0be7e
Emi <nicovilaviviano@gmail.com>
2024-09-21 14:19:21 -0300

    Reorden de funcion registrarUsuario y registroMascota

--- parte1.sh
+++ parte1.sh
@@ -51,61 +51,9 @@ option(){
             echo -e "4. Salir\n"
             read -p "Ingrese opcion: " option
             if [[ $option == 1 ]] ; then
-                prueba="no"
-                echo -e "\nIngrese el nombre del nuevo usuario:"
-                read newName
-                echo -e "\nIngrese la cedula del nuevo usuario:"
-                read newUser
-                yaExiste=false
-                while IFS=: read -r nombre cedula telefono fecha; do
-                    if [[ $newUser == $cedula ]] ; then
-                        yaExiste=true
-                    fi
-                done < users.txt
-                if [[ $yaExiste == false ]] ; then
-                    echo -e "\nIngrese el numero de telefono del nuevo usuario:"
-                    read newNum
-                    validDate=false
-                    while [[ $validDate == false ]] ; do
-                        echo -e "\nIngrese la fecha de nacimiento del nuevo usuario:"
-                        read newDate
-                        if [[ $newDate =~ ^([0-2][0-9]|3[0-1])/(0[1-9]|1[0-2])/[0-9]{4}$ ]] ; then
-                            validDate=true
-                        else
-                            echo -e "\nIngrese una fecha valida en formato dd/mm/aaaa"
-                        fi
-                    done
-                    iguales=false
-                    while [[ $iguales == false ]] ; do
-                        echo -e "\nIngrese la contraseña:"
-                        read -s newPass
-                        echo -e "\nIngrese nuevamente la contraseña:"
-                        read -s newPass2
-                        if [[ $newPass == $newPass2 ]] ; then
-                            iguales=true
-                        else
-                            echo -e "\nLas contraseñas no coinciden"
-                        fi
-                    done
-                    validOption=false
-                    while [[ $validOption == false ]] ; do
-                        echo -e "\n¿Desea que el nuevo usuario sea administrador?"
-                        echo "1. Si"
-                        echo -e "2. No\n"
-                        read -p "Ingrese opcion: " isAdmin
-                        if [[ $isAdmin == 1 || $isAdmin == 2 ]] ; then
-                            validOption=true
-                        else
-                            echo -e "\nIngrese una opcion valida"
-                        fi
-                    done
-                    echo "$newName:$newUser:$newNum:$newDate:$isAdmin:$newPass" >> users.txt
-                    echo -e "\nEl usuario fue registrado exitosamente"
-                else
-                    echo -e "\nYa existe un usuario registrado con esa cedula"
-                fi
+                registrarUsuario
             elif [[ $option == 2 ]] ; then
-                echo ""
+                registroMascota
             elif [[ $option == 3 ]] ; then
                 askOption=true
             elif [[ $option == 4 ]] ; then
@@ -122,6 +70,66 @@ option(){
         fi
 
     done
+}
+
+registrarUsuario(){
+    prueba="no"
+    echo -e "\nIngrese el nombre del nuevo usuario:"
+    read newName
+    echo -e "\nIngrese la cedula del nuevo usuario:"
+    read newUser
+    yaExiste=false
+    while IFS=: read -r nombre cedula telefono fecha; do
+        if [[ $newUser == $cedula ]] ; then
+            yaExiste=true
+        fi
+    done < users.txt
+    if [[ $yaExiste == false ]] ; then
+        echo -e "\nIngrese el numero de telefono del nuevo usuario:"
+        read newNum
+        validDate=false
+        while [[ $validDate == false ]] ; do
+            echo -e "\nIngrese la fecha de nacimiento del nuevo usuario:"
+            read newDate
+            if [[ $newDate =~ ^([0-2][0-9]|3[0-1])/(0[1-9]|1[0-2])/[0-9]{4}$ ]] ; then
+                validDate=true
+            else
+                echo -e "\nIngrese una fecha valida en formato dd/mm/aaaa"
+            fi
+        done
+        iguales=false
+        while [[ $iguales == false ]] ; do
+            echo -e "\nIngrese la contraseña:"
+            read -s newPass
+            echo -e "\nIngrese nuevamente la contraseña:"
+            read -s newPass2
+            if [[ $newPass == $newPass2 ]] ; then
+                iguales=true
+            else
+                echo -e "\nLas contraseñas no coinciden"
+            fi
+        done
+        validOption=false
+        while [[ $validOption == false ]] ; do
+            echo -e "\n¿Desea que el nuevo usuario sea administrador?"
+            echo "1. Si"
+            echo -e "2. No\n"
+            read -p "Ingrese opcion: " isAdmin
+            if [[ $isAdmin == 1 || $isAdmin == 2 ]] ; then
+                validOption=true
+            else
+                echo -e "\nIngrese una opcion valida"
+            fi
+        done
+        echo "$newName:$newUser:$newNum:$newDate:$isAdmin:$newPass" >> users.txt
+        echo -e "\nEl usuario fue registrado exitosamente"
+    else
+        echo -e "\nYa existe un usuario registrado con esa cedula"
+    fi
+}
+
+registroMascota(){
+    
 }
 
 echo "Bienvenido al sistema, inicie sesión"
